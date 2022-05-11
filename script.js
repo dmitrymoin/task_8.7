1d3a203833847614e5abb6ecec23a1fe253b0830
dmitrymoin <mdg.link@gmail.com>
2022-05-11 13:23:45 +0300

    add styles,update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
-let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+let minValue = parseInt(prompt('Минимальное знание числа для игры','0')) || 0;
+let maxValue = parseInt(prompt('Максимальное знание числа для игры','100')) || 100;
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
@@ -11,10 +11,9 @@ const answerField = document.getElementById('answerField');
 orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
 
+
 document.getElementById('btnRetry').addEventListener('click', function () {
     
-    // minValue = 0;
-    // maxValue = 100;
     minValue = parseInt(prompt('Минимальное знание числа для игры','0')) || 0;
     maxValue = parseInt(prompt('Максимальное знание числа для игры','100')) || 100;
     alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
@@ -29,11 +28,18 @@ document.getElementById('btnRetry').addEventListener('click', function () {
 document.getElementById('btnOver').addEventListener('click', function () {
     if (gameRun){
         if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
-            const answerPhrase = (phraseRandom === 1) ?
-                `Вы загадали неправильное число!\n\u{1F914}` :
-                `Я сдаюсь..\n\u{1F92F}`;
-
+            const phraseRandom = Math.round( Math.random()*2);
+            // const answerPhrase = (phraseRandom === 1) ?
+                // `Вы загадали неправильное число!\n\u{1F914}` :
+                // `Я сдаюсь..\n\u{1F92F}`;
+            let answerPhrase;
+                if (phraseRandom === 0){
+                    answerPhrase = `Что-то тут нечисто \u{1F914}`;
+                } else if (phraseRandom === 1){
+                    answerPhrase = `А не врешь ли ты часом? \u{1F925}`;                
+                } else if (phraseRandom === 2){
+                    answerPhrase = `Кто-то забыл какое число загадал`;
+                }                         
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else {
@@ -49,15 +55,19 @@ document.getElementById('btnOver').addEventListener('click', function () {
 document.getElementById('btnLess').addEventListener('click', function () {
     if (gameRun){
         if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
-            const answerPhrase = (phraseRandom === 1) ?
-                `Вы загадали неправильное число!\n\u{1F914}` :
-                `Я сдаюсь..\n\u{1F92F}`;
-
+            const phraseRandom = Math.round( Math.random()*2);
+            let answerPhrase;
+            if (phraseRandom === 0){
+                answerPhrase = `1AAAAAAAAAAA`;
+            } else if (phraseRandom === 1){
+                answerPhrase = `2AAAAAAAAAAA`;                
+            } else if (phraseRandom === 2){
+                answerPhrase = `3AAAAAAAAAAA`;
+            }                   
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else {
-            maxValue = answerNumber  + 1;
+            maxValue = answerNumber;
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
@@ -68,8 +78,17 @@ document.getElementById('btnLess').addEventListener('click', function () {
 
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun){
-        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
+        const phraseRandom = Math.round( Math.random()*2);
+        let answerPhrase;
+                if (phraseRandom === 0){
+                    answerPhrase = `Плёвое дело!`;
+                } else if (phraseRandom === 1){
+                    answerPhrase = `Это было легко!`;                
+                } else if (phraseRandom === 2){
+                    answerPhrase = `Проще простого \u{1F61C}`;
+                }
+        answerField.innerText = answerPhrase;
         gameRun = false;
-    }
+    }          
 })
 
